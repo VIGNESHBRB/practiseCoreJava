e56af46c903c32e3ed09f82c00cff8209bc594d4
Vignesh Babu B R B <20576119+VIGNESHBRB@users.noreply.github.com>
2018-11-23 08:45:41 +0530

    Practises
    1. serialization
    2. equals and hashcode
    
    Removed unwanted IDE files from repository

--- com/brb/main/Employee.java
+++ com/brb/main/Employee.java
@@ -1,20 +1,39 @@
 package com.brb.main;
 
-import java.util.Comparator;
+import java.io.Serializable;
+import java.util.Objects;
 
-public class Employee {
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 9884838998L;
 
     Employee(String name, int age){
         this.name = name;
         this.age = age;
     }
 
-    private String name;
+    // Do not want to serialize any data member of the class
+    private transient String name;
     int age;
 
 
     @Override
     public String toString(){
         return this.name + this.age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this) return true;
+        if(!(o instanceof Employee)) return false;
+
+        Employee emp = (Employee) o;
+
+        return (emp.age == this.age) && (emp.name.equals(this.name));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
--- com/brb/main/MainClass.java
+++ com/brb/main/MainClass.java
@@ -1,37 +1,57 @@
 package com.brb.main;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class MainClass {
 
     public static void main(String[] args) {
-       /* ThreadClass1 threadClass = new ThreadClass1();
-        ThreadClass2 threadClass2 = new ThreadClass2();
-        ThreadClass3 threadClass3 = new ThreadClass3();
-        Thread t1, t2;
-        t1 = new Thread(threadClass);
-        t2 = new Thread(threadClass2);
-        t1.start();
-        t2.start();
-        threadClass3.start();
-        System.out.println("Completed Threads");
-*/
-        Movie m = new Movie("Avatar", 2010);
-        Movie m2 = new Movie("Sarkar", 2018);
-        Movie m3 = new Movie("NNN", 1982);
+        practiseThreads();
+        practiseComparable();
+        practiseComparator();
+        serializationAndDeserialization();
+        practiseEqualsAndHashcode();
+    }
 
-        List<Movie> list = new ArrayList<>();
-        list.add(m);
-        list.add(m2);
-        list.add(m3);
+    private static void practiseEqualsAndHashcode() {
+        Set<Employee> equalsTestSet = new HashSet<>();
+        Employee e1 = new Employee("VB", 25);
+        Employee e2 = new Employee("VB", 25);
 
-        System.out.println(list);
-        // sorted using comparable
-        Collections.sort(list);
-        System.out.println(list);
+        equalsTestSet.add(e1);
+        equalsTestSet.add(e2);
 
+        System.out.println(equalsTestSet.size());
+    }
+
+    private static void serializationAndDeserialization() {
+        Employee serEmp = new Employee("Vignesh Babu B R B", 25);
+        try {
+            FileOutputStream fileOut = new FileOutputStream("serializeTest.txt");
+            ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+            objOut.writeObject(serEmp);
+            objOut.close();
+            fileOut.close();
+            System.out.println("Serialized an Object");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Employee deSerialEmp = null;
+        try {
+            FileInputStream fileOut = new FileInputStream("serializeTest.txt");
+            ObjectInputStream objOut = new ObjectInputStream(fileOut);
+            deSerialEmp = (Employee) objOut.readObject();
+            objOut.close();
+            fileOut.close();
+            System.out.println("DeSerialized an Object");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(deSerialEmp);
+    }
+
+    private static void practiseComparator() {
         Employee e = new Employee("VB", 25);
         Employee e2 = new Employee("Sanji", 50);
         Employee e3 = new Employee("SD", 24);
@@ -46,7 +66,34 @@ public class MainClass {
         EmployeeComparator comparator = new EmployeeComparator();
         Collections.sort(list1, comparator);
         System.out.println(list1);
+    }
 
+    private static void practiseComparable() {
+        Movie m = new Movie("Avatar", 2010);
+        Movie m2 = new Movie("Sarkar", 2018);
+        Movie m3 = new Movie("NNN", 1982);
+
+        List<Movie> list = new ArrayList<>();
+        list.add(m);
+        list.add(m2);
+        list.add(m3);
+
+        System.out.println(list);
+        // sorted using comparable
+        Collections.sort(list);
+        System.out.println(list);
+    }
+
+    private static void practiseThreads() {
+        ThreadClass1 threadClass = new ThreadClass1();
+        ThreadClass2 threadClass2 = new ThreadClass2();
+        ThreadClass3 threadClass3 = new ThreadClass3();
+        Thread t1, t2;
+        t1 = new Thread(threadClass);
+        t2 = new Thread(threadClass2);
+        t1.start();
+        t2.start();
+        threadClass3.start();
         System.out.println("Completed Threads");
     }
 }
